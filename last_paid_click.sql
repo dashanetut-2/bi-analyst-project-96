3ab734de5f27ed1d83dc2f4491addcacd1f63b33
dashanetut-2 <daatuutt@gmail.com>
2026-09-07 17:35:05 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,31 +1,50 @@
-SELECT
-    s.visitor_id,
-    s.visit_date,
-    s.source AS utm_source,
-    s.medium AS utm_medium,
-    s.campaign AS utm_campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id
-FROM sessions AS s
-LEFT JOIN leads AS l
-    ON s.visitor_id = l.visitor_id
-    AND l.created_at >= s.visit_date
-WHERE s.medium IN (
-    'cpc',
-    'cpm',
-    'cpa',
-    'youtube',
-    'cpp',
-    'tg',
-    'social'
+WITH lead_visits AS (
+    SELECT
+        s.visitor_id,
+        s.visit_date,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        ROW_NUMBER() OVER (
+            PARTITION BY l.lead_id
+            ORDER BY s.visit_date DESC
+        ) AS rn
+    FROM sessions AS s
+    LEFT JOIN leads AS l
+        ON s.visitor_id = l.visitor_id
+        AND l.created_at >= s.visit_date
+    WHERE s.medium IN (
+        'cpc',
+        'cpm',
+        'cpa',
+        'youtube',
+        'cpp',
+        'tg',
+        'social'
+    )
 )
+SELECT
+    visitor_id,
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
+FROM lead_visits
+WHERE rn = 1
 ORDER BY
-    l.amount DESC NULLS LAST,
-    s.visit_date ASC,
-    s.source ASC,
-    s.medium ASC,
-    s.campaign asc
-	limit 10;
+    amount DESC NULLS LAST,
+    visit_date ASC,
+    utm_source ASC,
+    utm_medium ASC,
+    utm_campaign ASC
+LIMIT 10;
